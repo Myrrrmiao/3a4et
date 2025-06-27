879c90422f98291bda1146fd10d000b876df4b5e
Myrrrmiao <myrrr759@gmail.com>
2025-06-27 20:22:09 +0300

    Add files via upload

---
+++ count_dgt.md.py
@@ -0,0 +1,13 @@
+def count_cifra(chislo):
+    cifra_counts = {}  #создаём словарь, в котором хранится количество каждой цифпы
+
+    for cifra in str(chislo):  # превращаем чсило в строку и проходимcя по каждой цифре/символу в строке
+        if cifra in cifra_counts: # чекаем есть ли эта цифра уже в словаре
+            cifra_counts[cifra] += 1  # если цифра есть, увеличиваем счётчик
+        else:
+            cifra_counts[cifra] = 1   # если нет, то добавляем с начальным значением один
+
+    return cifra_counts
+
+number = 567890876567890
+print(count_cifra(number))
