80da33bcba08dcf4854712f99acea73a6dfe3ef6
Myrrrmiao <myrrr759@gmail.com>
2025-06-28 13:38:57 +0300

    Add files via upload

---
+++ iter_flatteener.md.py
@@ -0,0 +1,27 @@
+class FlattenIterator:
+
+    def __init__(self, nested_list):
+        self.iterators = [iter(nested_list)]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.iterators:
+            try:
+                item = next(self.iterators[-1])
+            except StopIteration:
+                self.iterators.pop()
+                continue
+
+            if isinstance(item, list):
+                self.iterators.append(iter(item))
+                continue
+
+            return item
+
+        raise StopIteration
+
+nested_list = [1, [2, [3, 4], 5], 6]
+for num in FlattenIterator(nested_list):
+    print(num)
