72a2c8960b776e2ea231dac812585e38db312267
Myrrrmiao <myrrr759@gmail.com>
2025-06-28 13:56:36 +0300

    Add files via upload

---
+++ pascal.md.py
@@ -0,0 +1,15 @@
+def get_row(row_index):
+    row = [0] * (row_index + 1)
+    row[0] = 1
+
+    for current_row_number in range(1, row_index + 1):
+        for position_in_row in range(current_row_number, 0, -1):
+            row[position_in_row] = row[position_in_row] + row[position_in_row - 1]
+
+    return row
+
+print(get_row(0))
+print(get_row(1))
+print(get_row(2))
+print(get_row(3))
+print(get_row(4))  
